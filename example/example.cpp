eb2d8268c33e1996c7bc42b49fc9ad72e47120aa
Gabi Melman <gmelman1@gmail.com>
2019-09-30 13:09:28 +0300

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -4,21 +4,6 @@
 
 // spdlog usage example
 
-#include <cstdio>
-
-void stdout_logger_example();
-void basic_example();
-void rotating_example();
-void daily_example();
-void async_example();
-void binary_example();
-void trace_example();
-void multi_sink_example();
-void user_defined_example();
-void err_handler_example();
-void syslog_example();
-
-
 #include "spdlog/cfg/env.h"
 #include "spdlog/spdlog.h"
 
@@ -26,8 +11,6 @@ void syslog_example();
 
 int main(int, char *[])
 {
-    using namespace spdlog;
-    using namespace spdlog::sinks;
     spdlog::cfg::init_from_env();    
     auto l1 = spdlog::stderr_color_mt("logger1");
     auto l2 = spdlog::stderr_color_mt("logger2");
